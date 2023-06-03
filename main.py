769fe6b2169e00e26f8c147725f938c648fa72ae
Kishore112201035 <112201035@smail.iitpkd.ac.in>
2023-06-04 00:33:22 +0530

    Made some changes in animations of piano Board

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ is_visible = False
 game = Window(800, 600)
 
 menu_batch = pyglet.graphics.Batch()
-drum_set_batch = pyglet.graphics.Batch()
 background = Sprite(background_img)
 logo = Sprite(logo_img, batch=menu_batch)
 loading = Sprite(loading_img, batch=menu_batch)
 drums = Sprite(drums_img, batch=menu_batch)
+tabala = Sprite(tabala_img, batch=menu_batch)
 piano = Sprite(piano_img, batch=menu_batch)
+black_spot = Sprite(black_spot_img)
+white_spot = Sprite(white_spot_img)
+
 drum_set = DrumSet()
 piano_board = PianoBoard()
 
@@ -25,20 +28,27 @@ loading.scale = 0.3
 loading.opacity = 120
 loading.position = 400 - (loading.width / 2), 80, 0
 drums.scale = 1.1
-drums.position = 400 - (drums.width / 2), 320, 0
+drums.position = 200 - (drums.width / 2), 320, 0
 piano.position = 400 - (piano.width / 2), 20, 0
+tabala.position = 600 - (tabala.width / 2), 320,0
+
 
 
 drums.visible = False
 piano.visible = False
+tabala.visible = False
 drum_set.visible = False
 piano_board.visible = False
+black_spot.visible = False
+white_spot.visible = False
+
 
 
 def show_menu(dt):
     global is_visible
     drums.visible = True
     piano.visible = True
+    tabala.visible = True
     logo.visible = False
     loading.visible = False
 
@@ -55,14 +65,20 @@ def on_draw():
     menu_batch.draw()
     drum_set.draw()
     piano_board.draw()
+    black_spot.draw()
+    white_spot.draw()
 
 @game.event
 def on_key_press(symbol, modifier):
     if drum_set.visible:
         drum_set.key_press(symbol)
     if piano_board.visible:
-        piano_board.key_press(symbol)
+        piano_board.key_press(symbol, black_spot, white_spot)
 
+@game.event
+def on_key_release(symbol, modifier):
+    if piano_board.visible:
+        piano_board.key_release(symbol, black_spot, white_spot)
 
 @game.event
 def on_mouse_motion(x, y, dx, dy):
@@ -75,6 +91,11 @@ def on_mouse_motion(x, y, dx, dy):
         piano.scale = 1.1
     else:
         piano.scale = 1
+
+    if on_sprite(tabala, x, y):
+        tabala.scale = 1.2
+    else:
+        tabala.scale = 1.1
 
 
 
--- piano_board.py
+++ piano_board.py
@@ -1,7 +1,9 @@
+import pyglet.graphics
 from pyglet.sprite import Sprite
 from pyglet.media import *
 from pyglet.window.key import *
-from resourses import piano_board_img
+from resourses import piano_board_img, black_spot_img
+
 
 
 def play_sound(sound_file):
@@ -14,32 +16,92 @@ class PianoBoard(Sprite):
     def __init__(self):
         super().__init__(piano_board_img)
 
-    def key_press(self,symbol):
+    def key_press(self, symbol, bl, ws):
         if symbol == Z:
             play_sound('assets/sfx/piano_sfx/Bo.mp3')
+            bl.scale = 2.7
+            bl.position = 0, 30, 0
+            bl.visible = True
         if symbol == X:
             play_sound('assets/sfx/piano_sfx/C2.mp3')
+            bl.scale = 2.7
+            bl.position = 114, 30, 0
+            bl.visible = True
         if symbol == C:
             play_sound('assets/sfx/piano_sfx/E3.mp3')
+            bl.scale = 2.7
+            bl.position = 229, 30, 0
+            bl.visible = True
         if symbol == V:
             play_sound('assets/sfx/piano_sfx/F4.mp3')
+            bl.scale = 2.7
+            bl.position = 343, 30, 0
+            bl.visible = True
         if symbol == B:
             play_sound('assets/sfx/piano_sfx/G5.mp3')
+            bl.scale = 2.7
+            bl.position = 457, 30, 0
+            bl.visible = True
         if symbol == N:
             play_sound('assets/sfx/piano_sfx/C6.mp3')
+            bl.scale = 2.7
+            bl.position = 571, 30, 0
+            bl.visible = True
         if symbol == M:
             play_sound('assets/sfx/piano_sfx/G6.mp3')
+            bl.scale = 2.7
+            bl.position = 686, 30, 0
+            bl.visible = True
+        if symbol == S:
+            play_sound('assets/sfx/piano_sfx/sec key.mp3')
+            ws.scale = 1.7
+            ws.position = 68, 250, 0
+            ws.visible = True
+        if symbol == D:
+            play_sound('assets/sfx/piano_sfx/Bb-[AudioTrimmer.com].mp3')
+            ws.scale = 1.7
+            ws.position = 194, 250, 0
+            ws.visible = True
+        if symbol == G:
+            play_sound('assets/sfx/piano_sfx/c#-[AudioTrimmer.com].mp3')
+            ws.scale = 1.7
+            ws.position = 408, 250, 0
+            ws.visible = True
+        if symbol == H:
+            play_sound('assets/sfx/piano_sfx/d#-[AudioTrimmer.com].mp3')
+            ws.scale = 1.7
+            ws.position = 533, 250, 0
+            ws.visible = True
+        if symbol == J:
+            play_sound('assets/sfx/piano_sfx/g#-[AudioTrimmer.com].mp3')
+            ws.scale = 1.7
+            ws.position = 660, 250, 0
+            ws.visible = True
 
 
-        # if symbol == S:
-        #     play_sound('assets/sfx/piano_sfx/F1.mp3')
-        # if symbol == D:
-        #     play_sound('assets/sfx/piano_sfx/Bb-[AudioTrimmer.com].mp3')
-        # if symbol == G:
-        #     play_sound('assets/sfx/piano_sfx/c#-[AudioTrimmer.com].mp3')
-        # if symbol == H:
-        #     play_sound('assets/sfx/piano_sfx/d#-[AudioTrimmer.com].mp3')
-        # if symbol == J:
-        #     play_sound('assets/sfx/piano_sfx/g#-[AudioTrimmer.com].mp3')
-
+    def key_release(self, symbol, bl, ws):
+        if symbol == Z:
+            bl.visible = False
+        if symbol == X:
+            bl.visible = False
+        if symbol == C:
+            bl.visible = False
+        if symbol == V:
+            bl.visible = False
+        if symbol == B:
+            bl.visible = False
+        if symbol == N:
+            bl.visible = False
+        if symbol == M:
+            bl.visible = False
+        if symbol == S:
+            ws.visible = False
+        if symbol == D:
+            ws.visible = False
+        if symbol == G:
+            ws.visible = False
+        if symbol == H:
+            ws.visible = False
+        if symbol == J:
+            ws.visible = False
 
